6893dee9c68c18b0c0d252f2d010d64742e1c3f4
minisera <koki.ohtaka0821@gmail.com>
2021-11-27 16:03:28 +0900

    httpクライアントでデータを取得するよう変更

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router'
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { MembersComponent } from './members/members.component';
+import { MemberDetailComponent } from './member-detail/member-detail.component';
 
 const routes: Routes = [
   { path: '', redirectTo: 'dashboard', pathMatch: 'full'},
   { path: 'dashboard', component: DashboardComponent},
-  { path: 'members', component: MembersComponent}
+  { path: 'members', component: MembersComponent},
+  { path: 'detail/:id', component: MemberDetailComponent}
 ];
 
 @NgModule({
